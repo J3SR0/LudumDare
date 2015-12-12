16b69a449184df2a13fd17699b5a6d2cd1606501
magne_w <wilfried.magne@epitech.eu>
2015-12-12 06:50:06 +0100

    template

--- LundumDare/Assets/_Scripts/Classes/Characters/Player.cs
+++ LundumDare/Assets/_Scripts/Classes/Characters/Player.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Player : Character {
 
+	private float h;
+	private float v;
+
 	// Use this for initialization
 	public override void Start () {
 		base.input = this.transform.parent.parent.GetComponent<InputHandler>();
@@ -14,6 +17,13 @@ public class Player : Character {
 	}
 
 	public override void Move () {
+		h = base.input.h;
+		v = base.input.v;
+		if (h < 0) {
+			Debug.Log("LEL");
+		} else if (h > 0) {
+			Debug.Log("LOL");
+		}
 	}
 
 	public override void Attack () {
