93fcfa13fa9bc8007e67de284f2188ce96945a3a
Tazrof <tazrof91@gmail.com>
2015-12-13 14:29:19 +0100

    Epure blink function
    Function

--- LundumDare/Assets/_Scripts/Utility.cs
+++ LundumDare/Assets/_Scripts/Utility.cs
@@ -6,16 +6,13 @@ public class Utility : MonoBehaviour {
 	private bool isBlinking = false;
 
 	public void blink (GameObject gameObjectToBlink = null, float showTime = 1f, float hideTime = 1f, float timer = 0f) {
-		Debug.Log ("--- Start ---");
-		Debug.Log ("gameObject: " + gameObjectToBlink);
-		Debug.Log ("isBlinking: " + isBlinking);
 		if (gameObjectToBlink != null && !isBlinking) {
 			StartCoroutine (blinking (gameObjectToBlink, showTime, hideTime, timer));
 			isBlinking = true;
 		}
 	}
 
-	IEnumerator blinking (GameObject gameObjectToBlink, float showTime, float hideTime, float timer) {
+	private IEnumerator blinking (GameObject gameObjectToBlink, float showTime, float hideTime, float timer) {
 		if (timer == 0f) {
 			while (true) {
 				gameObjectToBlink.GetComponent<Renderer> ().enabled = false;
