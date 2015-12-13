5ed782ff678fa83b0f3d10721b1a8ea80d4a7738
mikaza22 <geraldine.pbf@gmail.com>
2015-12-13 12:32:38 +0100

    petit menu

---
+++ LundumDare/Assets/_Scripts/Menu.js
@@ -0,0 +1,18 @@
+﻿
+
+function OnGUI() {
+
+    if (GUI.Button(Rect(Screen.width / 2 - 100, Screen.height / 2 - 80, 200, 50), "Jouer")) {
+        Application.LoadLevel("Level 1");
+    }
+
+    if (GUI.Button(Rect(Screen.width / 2 - 100, Screen.height / 2 - 20, 200, 50), "Instructions")) {
+
+    }
+
+    if (GUI.Button(Rect(Screen.width / 2 - 100, Screen.height / 2 - -40, 200, 50), "Quitter")) {
+        Application.Quit();
+
+    }
+
+}
