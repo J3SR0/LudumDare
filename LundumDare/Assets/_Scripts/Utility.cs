32a2d42cbb28c3fe3cc1d14d4cf78f468f6ee191
Tazrof <tazrof91@gmail.com>
2015-12-13 15:16:28 +0100

    Epure script

--- LundumDare/Assets/_Scripts/Utility.cs
+++ LundumDare/Assets/_Scripts/Utility.cs
@@ -5,6 +5,9 @@ public class Utility : MonoBehaviour {
 
 	private bool isBlinking = false;
 
+	///<summary>
+	///This function allow to blink a gameobject.
+	///</summary>
 	public void blink (GameObject gameObjectToBlink = null, float showTime = 1f, float hideTime = 1f, float timer = 0f) {
 		if (gameObjectToBlink != null && !isBlinking) {
 			StartCoroutine (blinking (gameObjectToBlink, showTime, hideTime, timer));
@@ -17,8 +20,10 @@ public class Utility : MonoBehaviour {
 			while (true) {
 				gameObjectToBlink.GetComponent<Renderer> ().enabled = false;
 				yield return new WaitForSeconds (hideTime);
+				hideTime -= 0.01f;
 				gameObjectToBlink.GetComponent<Renderer> ().enabled = true;
 				yield return new WaitForSeconds (showTime);
+				showTime -= 0.01f;
 			}
 		} else {
 			timer += Time.time;
