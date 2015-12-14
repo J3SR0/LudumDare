c333d8f61307da2ba120207faaf95e95ff3fb063
Tazrof <tazrof91@gmail.com>
2015-12-14 11:06:26 +0100

    Modification for laser

--- LundumDare/Assets/_Scripts/Laser.cs
+++ LundumDare/Assets/_Scripts/Laser.cs
@@ -5,81 +5,42 @@ public class Laser : MonoBehaviour {
 
 	[SerializeField]
 	private GameObject target = null;
-	public GameObject Target {
-		set {target = value;}
-	}
+	public GameObject Target { set { target = value; } }
 
 	[SerializeField]
 	private float speed = 5f;
-
-	[SerializeField]
-	private float timerBeforeDeath = 5f;
-
+	public float Speed { get { return speed; } set { speed = value; } }
 	[SerializeField]
 	private float timerBeforeShot = 2f;
-	public float TimeBeforeShot { get; set; }
+	public float TimeBeforeShot { get { return timerBeforeShot; } set { timerBeforeShot = value; } }
 
 	[SerializeField]
 	private float frequency = 0.02f;
-	public float Frequency { get; set; }
+	public float Frequency { get { return frequency;} set { frequency = value; } }
 
 	[SerializeField]
 	private float coolDown = 10f;
-	public float CoolDown { get; set; }
+	public float CoolDown { get { return coolDown; } set { coolDown = value; } }
 
-	private float originalY, y, myColliderSizeY;
-
-	private GameObject lastCollision = null;
+	public GameObject GameObject { get { return gameObject; } }
 
 	void Start () {
-		originalY = transform.position.y;
-		y = originalY;
-		myColliderSizeY = GetComponent<Collider> ().bounds.size.y / 2f;
-		timerBeforeDeath += Time.time;
 		timerBeforeShot += Time.time;
 	}
 		
 	void Update () {
-		if (lastCollision != null) {
-			if (isTouching()) {
-				y = lastCollision.transform.position.y + (lastCollision.GetComponent<Collider> ().bounds.size.y / 2f) + myColliderSizeY;
-				transform.position = new Vector2 (transform.position.x, y);
-			}
-			else {
-				y = originalY;
-				transform.position = new Vector2 (transform.position.x, y);
-			}
+		float currentTime = Time.time;
+		if (currentTime >= timerBeforeShot) {
+			Shoot ();
+			if (currentTime >= timerBeforeShot + 1f)
+				Destroy (gameObject);
 		}
-		if (target != null)
-			transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.transform.position.x, y), speed * Time.deltaTime);
-		if (Time.time >= timerBeforeDeath)
-			Destroy (gameObject);
-		else if (Time.time >= timerBeforeShot)
-			Shoot ();		
-	}
-
-	void OnCollisionEnter(Collision collision) {
-		lastCollision = collision.gameObject;
-	}
-
-	private bool isTouching() {
-		float lastCollisionColliderSizeY = lastCollision.GetComponent<Collider> ().bounds.size.y / 2f;
-		float lastCollisionColliderSizeX = lastCollision.GetComponent<Collider> ().bounds.size.x / 2f;
-
-		float impactY = transform.position.y - myColliderSizeY - 0.5f;
-		float realImpactY = lastCollision.transform.position.y + lastCollisionColliderSizeY;
-		float leftPoint = lastCollision.transform.position.x - lastCollisionColliderSizeX - 2f;
-		float rightPoint = lastCollision.transform.position.x + lastCollisionColliderSizeX + 2f;
-		float positionX = transform.position.x;
-
-		if (!(impactY <= realImpactY && leftPoint <= positionX && positionX <= rightPoint))
-			return false;
-		else
-			return true;
+		else if (target != null)
+			transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.transform.position.x, transform.position.y), speed * Time.deltaTime);
 	}
 
 	private void Shoot() {
-		Debug.Log ("Shot!");
+		transform.localPosition = new Vector3(transform.localPosition.x, -19f, 0);
 	}
 		
 }
