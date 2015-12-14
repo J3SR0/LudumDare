e7d1f3d76acab667ea4a1ec06a425315518998cb
mikaza22 <geraldine.pbf@gmail.com>
2015-12-14 22:37:24 +0100

    menu
    menu

--- LundumDare/Assets/_Scripts/MenuScript.cs
+++ LundumDare/Assets/_Scripts/MenuScript.cs
@@ -13,6 +13,10 @@ public class MenuScript : MonoBehaviour
         GUIStyle myStyle = new GUIStyle(GUI.skin.GetStyle("button"));
         myStyle.fontSize = 32;
         myStyle.font = font;
+        GUIStyle myStyle1 = new GUIStyle(GUI.skin.GetStyle("label"));
+        myStyle1.fontSize = 40;
+        myStyle1.font = font;
+        GUI.Label(new Rect(Screen.width / 2 - (500 / 2), (2 * Screen.height / 3) - 500, 500, 100), "D A S H M A G E D D O N", myStyle1);
         if (GUI.Button(new Rect(Screen.width / 2 - (400 / 2), (2 * Screen.height / 3) - 300, 400, 100), "P l a y", myStyle))
             Application.LoadLevel("StdTerrain");
         if (GUI.Button(new Rect(Screen.width / 2 - (400 / 2), (2 * Screen.height / 3) - 150, 400, 100), "R u l e s", myStyle))
---
+++ LundumDare/Assets/_Scripts/titlegame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Script de l'écran titre
+/// </summary>
+/// 
+
+public class titlegame : MonoBehaviour
+{
+    public Font font;
+    void OnGUI()
+    {
+        GUIStyle myStyle = new GUIStyle(GUI.skin.GetStyle("label"));
+        myStyle.fontSize = 40;
+        myStyle.font = font;
+        GUIStyle myStyle1 = new GUIStyle(GUI.skin.GetStyle("label"));
+        myStyle1.fontSize = 20;
+        myStyle1.font = font;
+        GUI.Label(new Rect(Screen.width / 2 - (300 / 2), (2 * Screen.height / 3) - 500, 600, 200), "D E V   B Y", myStyle);
+        GUI.Label(new Rect(Screen.width / 2 - (150 / 2), (2 * Screen.height / 3) - 300, 300, 100), "H E U S E   G", myStyle1);
+        GUI.Label(new Rect(Screen.width / 2 - (150 / 2), (2 * Screen.height / 3) - 200, 300, 100), "M A G N E   W", myStyle1);
+        GUI.Label(new Rect(Screen.width / 2 - (150 / 2), (2 * Screen.height / 3) - 100, 300, 100), "P A C C A R D   C", myStyle1);
+    }
+}
